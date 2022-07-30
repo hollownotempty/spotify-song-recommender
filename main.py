4e98d630e20563781a9e2d710d8b4e0eb5c341e6
Tom Holohan <81058831+hollownotempty@users.noreply.github.com>
2022-07-30 17:27:06 +0200

    Added playlist creation to functionality

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from genericpath import isfile
 from pathlib import Path
-from spotipy.oauth2 import SpotifyClientCredentials
+from spotipy.oauth2 import SpotifyOAuth
 
 import argparse
 import logging
@@ -12,14 +12,16 @@ if os.path.isfile('env.py'):
 
 
 def get_args():
-    parser = argparse.ArgumentParser(description='Recommendations for the '
-                                     'given artist')
-    parser.add_argument('-a', '--artist', required=True, help='Name of Artist')
+    parser = argparse.ArgumentParser(description='Creates a playlist for user')
+    parser.add_argument('-p', '--playlist', required=True,
+                        help='Name of Playlist')
+    parser.add_argument('-d', '--description', required=False, default='',
+                        help='Description of Playlist')
     return parser.parse_args()
 
 
 def get_artist(name):
-    results = sp.search(q='artist:' + name, type='artist')
+    results = sp.search(q='artist:' + name, type='artist', limit=50)
     items = results['artists']['items']
     if len(items) > 0:
         return items[0]
@@ -27,26 +29,37 @@ def get_artist(name):
         return None
 
 
-def show_recommendations_for_artist(artist):
+def get_recommendations_for_artist(artist):
     results = sp.recommendations(seed_artists=[artist['id']])
-    track = results['tracks'][0]
-    logger.info("You're gonna love '" + track['name'] + "' by " + track['artists'][0]['name'] + " - " + track['external_urls']['spotify'])
+    track_ids = []
+    for track in results['tracks']:
+        track_ids.append("spotify:track:" + track['id'])
+
+    return track_ids
+
+def create_new_playlist(playlist, track_ids):
+    sp.playlist_add_items(playlist, track_ids, position=None)
+    print("Playlist successfully created")
 
 
 def main():
     args = get_args()
-    artist = get_artist(args.artist)
+    user_id = sp.me()['id']
+    playlist = sp.user_playlist_create(user_id, args.playlist)
+    artist_name = input("Please enter an artist:\n")
+    artist = get_artist(artist_name)
     if artist:
-        show_recommendations_for_artist(artist)
+        track_ids = get_recommendations_for_artist(artist)
+        create_new_playlist(playlist['id'], track_ids)
     else:
         logger.error("Can't find that artist", args.artist)
+    
 
 
 if __name__ == '__main__':
     logger = logging.getLogger('examples.artist_recommendations')
     logging.basicConfig(level='INFO')
-
-    client_credentials_manager = SpotifyClientCredentials()
-    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+    scope = 'playlist-modify-public'
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
 
     main()
